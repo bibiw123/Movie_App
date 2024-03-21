42075ef2ffab6a1dd4c38113350bc83d1524ce74
NumericFactory <frederic.lossignol@gmail.com>
2024-03-21 18:16:02 +0100

    fix: auth api url

--- src/app/core/adapters/auth.service.ts
+++ src/app/core/adapters/auth.service.ts
@@ -7,34 +7,34 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService implements AuthGateway{
+export class AuthService implements AuthGateway {
 
-  apiurl=environment.API_URL
+  apiurl = environment.API_URL
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
 
   private _isAuth$ = new BehaviorSubject<boolean>(false);
-  public isAuth$: Observable<boolean>=this._isAuth$.asObservable()
+  public isAuth$: Observable<boolean> = this._isAuth$.asObservable()
 
-   register(user:any): Observable<any> {
-    const endpoint = "/api/v1/register"
-    return this.http.post(this.apiurl+endpoint,user)
+  register(user: any): Observable<any> {
+    const endpoint = "/register"
+    return this.http.post(this.apiurl + endpoint, user)
   }
 
-   login(user:any): Observable<any> {
+  login(user: any): Observable<any> {
     const endpoint = "/login"
-    return this.http.post(this.apiurl+endpoint,user)
+    return this.http.post(this.apiurl + endpoint, user)
   }
 
-   logout(): Observable<any> {
+  logout(): Observable<any> {
     const endpoint = "/logout"
-    return this.http.get(this.apiurl+endpoint)
+    return this.http.get(this.apiurl + endpoint)
   }
-   storeTokenInLocalStorage(): void {
+  storeTokenInLocalStorage(): void {
 
   }
-   getTokenFromLocalStorage(): string {
+  getTokenFromLocalStorage(): string {
     return ""
   }
 }
